4eadc6ac5c811bed6bb789c626e3e05190882370
Travis Vanderstad <t.vanderstad@live.com>
2015-08-31 15:39:26 -0400

    quick fix

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,7 @@ int main() {
 		depth = camera->retrieveMeasure(MEASURE::DEPTH);
 		imLeft = camera->retrieveImage(SIDE::LEFT);
 		cloud->fill(imLeft.data, (float*) depth.data, camera->getParameters());
+		cloud->genHeightMap(640, 640);
 		viewer->AddData(cloud);
 
 		// Update the value of key so that we can quit when the user strikes 'q'
