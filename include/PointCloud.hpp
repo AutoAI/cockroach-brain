b5656e6296488f1dda119a6805c5e433baa62170
Travis A. Vanderstad <travis@Traviss-MacBook-Pro.local>
2015-09-13 16:00:56 -0400

    quickfix

--- include/PointCloud.hpp
+++ include/PointCloud.hpp
@@ -3,6 +3,8 @@
 #pragma once
 
 #include <zed/utils/GlobalDefine.hpp>
+
+#include "HeightMap.hpp"
 #include "utils.hpp"
 
 class PointCloud {
@@ -11,7 +13,7 @@ public:
 	~PointCloud();
 
 	void fill(const unsigned char* image, const float* depth, const sl::zed::StereoParameters *param);
-	void genHeightMap(int width, int depth);
+	HeightMap* genHeightMap(int width, int depth);
 
 	POINT3D point(size_t i, size_t j);
 	POINT3D point(size_t i);
