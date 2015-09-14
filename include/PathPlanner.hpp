e1d444d4c0ea94e08e6e577b91cc4af76d2dd2f5
Travis A. Vanderstad <travis@Traviss-MacBook-Pro.local>
2015-09-14 02:07:10 -0400

    quickfix

--- include/PathPlanner.hpp
+++ include/PathPlanner.hpp
@@ -2,9 +2,9 @@
 
 #pragma once
 
-#define LOOKAHEAD_MIN 1.0f // meters
-#define LOOKAHEAD_MAX 3.0f
-#define EDGE_THRESHOLD .5f // [0, 1]
+#define LOOKAHEAD_MIN 1.5f // meters
+#define LOOKAHEAD_MAX 3.5f
+#define EDGE_THRESHOLD .4f // [0, 1]
 
 #include "HeightMap.hpp"
 
